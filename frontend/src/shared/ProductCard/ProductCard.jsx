afdc7dbcc1c5dc89b5d0fc8abaed9ab1f7d55bce
Vasyl Tkachenko <vasyltkachenko07@gmail.com>
2023-12-21 20:34:57 +0200

    The path to the pictures has been changed #7

--- frontend/src/shared/ProductCard/ProductCard.jsx
+++ frontend/src/shared/ProductCard/ProductCard.jsx
@@ -9,7 +9,7 @@ const ProductCard = ({ product }) => {
 	return (
 		<article className='product-card'>
 			<Link to={`/catalog/product/${product.id}`} className='product-card__image'>
-				<img src={`./${product.image}`} alt='' />
+				<img src={`../${product.image}`} alt='' />
 			</Link>
 			<div className='product-card__content'>
 				<Link to={`/catalog/product/${product.id}`} className='product-card__title'>
